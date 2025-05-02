c19d99ecefd60c58955f302f22ac74e54a9eed42
JBR-0100 <jbr.the.programmer@gmail.com>
2025-05-02 17:59:40 +0530

    final

--- auth.config.ts
+++ auth.config.ts
@@ -8,14 +8,11 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        // Always use absolute URL for redirects
-        const baseUrl = 'https://project-finance-dashboard.vercel.app';
-        return Response.redirect(new URL('/dashboard', baseUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
---
+++ next.config copy.ts
@@ -0,0 +1,27 @@
+// import type { NextConfig } from 'next';
+ 
+// const nextConfig: NextConfig = {
+
+// };
+ 
+// export default nextConfig;
+
+
+import type { NextConfig } from 'next';
+ 
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      'images.pexels.com',
+      'images.unsplash.com',
+      'imgur.com',
+      'i.imgur.com',
+      'picsum.photos',
+      'avatars.githubusercontent.com',
+      'res.cloudinary.com',
+      'st2.depositphotos.com'
+    ],
+  },
+};
+ 
+export default nextConfig;
